ed2880b810a00c94a579d3b36b3fe9328bf896b5
furkandndr <dundarfurkan35@outlook.com>
2023-10-07 21:43:26 +0300

    Image update

--- NarutoChar/Controllers/CharController.cs
+++ NarutoChar/Controllers/CharController.cs
@@ -17,13 +17,15 @@ public class CharController : ControllerBase
         HtmlDocument html = site.Load(link);
 
         IList<HtmlNode> nodes = html.QuerySelectorAll("div#mainframe1.outframe").QuerySelectorAll("ul.flexcontainer.zero li.flexitem.pad");
-        
-        
-        ;
+    
 
-        var data = nodes.Select((node)=>{
-            return new {
-                name = node.QuerySelector("p.pad.bo1 a").InnerText
+        var data = nodes.Select((node)=>
+        {
+            var imageNode = node.QuerySelector("div#mainframe1.outframe ul.flexcontainer.zero li.flexitem.pad a img.thumb100 ");
+            return new 
+            {
+                name = node.QuerySelector("p.pad.bo1 a").InnerText,
+                image_link = imageNode != null ? imageNode.GetAttributeValue("src", "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F792070653234421442%2F&psig=AOvVaw3MnJz2FggarzNZrD1a6FZR&ust=1696790185783000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCLCHsJTK5IEDFQAAAAAdAAAAABAE") : "Image not found"
             };
         });
         
